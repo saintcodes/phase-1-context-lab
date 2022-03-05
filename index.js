faca2dc4e9f5a0dca50ae2945beb4cfbad0ef16d
Michelle <clairsin@gmail.com>
2022-03-04 23:35:05 -0500

    commit tree 9392010105035d88520f6176a1f54ce855eeb6a3

--- index.js
+++ index.js
@@ -1,5 +1,70 @@
 /* Your Code Here */
 
+function createEmployeeRecord(employeeInformation) {
+    return {
+        firstName: employeeInformation[0],
+        familyName: employeeInformation[1],
+        title: employeeInformation[2],
+        payPerHour: employeeInformation[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+function createEmployeeRecords (recordsArray) {
+    const employeeRecords = recordsArray.map((record) => {
+      return createEmployeeRecord(record)
+      })
+      return employeeRecords
+    }
+    
+    function createTimeInEvent(dateStamp) {
+      let dateHourSplit = dateStamp.split(' ')
+      let date = dateHourSplit[0]
+      let hour = parseInt(dateHourSplit[1])
+    
+      let timeIn = {
+        type: "TimeIn",
+        hour: hour,
+        date: date,
+      }
+     this.timeInEvents.push(timeIn); 
+     return this
+    }  
+    
+    function createTimeOutEvent(dateStamp) {
+      let dateHourSplit = dateStamp.split(' ')
+      let date = dateHourSplit[0]
+      let hour = parseInt(dateHourSplit[1])
+    
+      let timeOut = {
+        type: "TimeOut",
+        hour: hour,
+        date: date,
+      }
+      this.timeOutEvents.push(timeOut);
+      return this
+    }
+    
+    function hoursWorkedOnDate(date) {
+      let timeInObjArray = this.timeInEvents;
+      console.log("timeInObjArray: ", timeInObjArray)
+      let timeOutObjArray = this.timeOutEvents;
+      let timeOut = timeOutObjArray.find(timeOutObj => timeOutObj.date === date)
+      let timeIn = timeInObjArray.find(timeInObj => timeInObj.date === date)
+      return (timeOut.hour - timeIn.hour)/100
+    }
+    
+    function wagesEarnedOnDate(date) {
+        console.log("THIS IS BS in wages: " , this)
+        // console.log("date: ", date)
+        // console.log("hours worked on date: ", hoursWorkedOnDate(date))
+        // console.log("this payperhour: ", call.payPerHour)
+        // hoursWorkedOnDate.bind(date)
+        return hoursWorkedOnDate(date) * recordObj.payPerHour
+      
+    }
+    
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -21,3 +86,7 @@ const allWagesFor = function () {
     return payable
 }
 
+
+// function findEmployeeByFirstName (srcArray, firstName) {
+//     let srcArray
+// }
